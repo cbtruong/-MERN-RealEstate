59c553d388bcfb7fa34948a540cf74f35da31d9b
cbtruong <batruong12122002@gmail.com>
2025-06-10 11:14:56 +0700

    create client copy

---
+++ client copy/src/App.jsx
@@ -0,0 +1,25 @@
+import{ BrowserRouter as Router, Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+const App = () => {
+  return (
+    <Router>
+      <Header/>
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
+App
