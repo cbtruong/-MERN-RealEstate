ca5ef6327edff3f877a506868998a355db9cacac
cbtruong <batruong12122002@gmail.com>
2025-06-11 11:54:44 +0700

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRouter= require('./routes/user.route');
 const authRouter=require('./routes/auth.route');
+const cookieParser = require("cookie-parser");
 
 dotenv.config();
 
@@ -17,8 +18,11 @@ mongoose.connect(process.env.MONGO)
 const app=express();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
+
 
 app.listen(3000,(req,res)=>{
     console.log('Server is running on port 3000!')
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 const express= require("express");
 const router= express.Router();
 
-const {test}= require('../controllers/user.controller');
+const {verifyToken} = require('../utils/verifyUser.js');
+const {test, updateUser}= require('../controllers/user.controller');
 
 router.get("/",test);
+router.post("/update/:id",verifyToken,updateUser);
 
 module.exports= router;
