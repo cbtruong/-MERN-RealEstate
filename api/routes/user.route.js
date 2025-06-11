c08313c9dfe159e9d61a5934d3a3b85c9bbb7ab7
cbtruong <batruong12122002@gmail.com>
2025-06-12 05:57:17 +0700

    create get user listing api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,10 +2,11 @@ const express= require("express");
 const router= express.Router();
 
 const {verifyToken} = require('../utils/verifyUser.js');
-const {test, updateUser,deleteUser}= require('../controllers/user.controller');
+const {test, updateUser,deleteUser,getUserListings}= require('../controllers/user.controller');
 
 router.get("/",test);
 router.post("/update/:id",verifyToken,updateUser);
 router.delete("/delete/:id",verifyToken,deleteUser);
+router.get("/listing/:id",verifyToken,getUserListings);
 
 module.exports= router;
