eb2445a42b83ba289982bbf9f16ffbdfc39390f3
cbtruong <batruong12122002@gmail.com>
2025-06-12 08:14:29 +0700

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,9 +2,10 @@ const express= require("express");
 const router= express.Router();
 
 const {verifyToken}= require('../utils/verifyUser');
-const {createListing,deleteListing}= require('../controllers/listing.controller');
+const {createListing,deleteListing,updateListing}= require('../controllers/listing.controller');
 
 router.post('/create',verifyToken,createListing);
 router.delete('/delete/:id',verifyToken,deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
 
 module.exports= router;
