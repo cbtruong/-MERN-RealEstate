65439b4a86e2a60ad0bc22becd3ba257d0211ce4
cbtruong <batruong12122002@gmail.com>
2025-06-12 09:11:44 +0700

    Create search API route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,11 +2,12 @@ const express= require("express");
 const router= express.Router();
 
 const {verifyToken}= require('../utils/verifyUser');
-const {createListing,deleteListing,updateListing,getListing}= require('../controllers/listing.controller');
+const {createListing,deleteListing,updateListing,getListing,getListings}= require('../controllers/listing.controller');
 
 router.post('/create',verifyToken,createListing);
 router.delete('/delete/:id',verifyToken,deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
+router.get('/get', getListings);
 
 module.exports= router;
